ff8f9dd9ddb8aedbe1c73c957accfb7eb1edc9ec
Jakobi Matagi <jkmatagi@hotmail.com>
2021-10-03 20:48:15 -0600

    cursor pointer

--- components/Header.js
+++ components/Header.js
@@ -21,12 +21,12 @@ function Header() {
                 p-2 cursor-pointer md:mx-2"/>
             </div>
         {/* Header Right */}
-            <div className="flex items-center space-x-4 justify-end text-gray-500">
+            <div className="flex items-center space-x-4 justify-end text-gray-500 ">
                 <p className="hidden md:inline-flex cursor-pointer">Become a host</p>
-                <GlobeAltIcon className="h-6 "/>
+                <GlobeAltIcon className="h-6 cursor-pointer"/>
                 <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-                    <MenuIcon className="h-6"/>
-                    <UserCircleIcon className="h-6"/>
+                    <MenuIcon className="h-6 cursor-pointer"/>
+                    <UserCircleIcon className="h-6 cursor-pointer"/>
                 </div>
             </div>
         </header>
