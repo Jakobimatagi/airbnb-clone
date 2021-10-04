2bee04e9b02f3fe17846a0ed6e2fe674c83bd000
Jakobi Matagi <jkmatagi@hotmail.com>
2021-10-03 19:32:45 -0600

    search bar created

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-
+import { SearchIcon } from '@heroicons/react/solid'
 function Header() {
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5">
@@ -13,8 +13,13 @@ function Header() {
                   objectPosition="left"
                 />
             </div>
-        {/* Header Middle */}
-            <div></div>
+        {/* Header Middle - Search */}
+            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                <input type="text" placeholder="Start your search" className="flex-grow pl-5 bg-transparent 
+                outline-none text-sm text-gray-600 placeholder-gray-400"/>
+                <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full 
+                p-2 cursor-pointer md:mx-2"/>
+            </div>
         {/* Header Right */}
             <div></div>
         </header>
