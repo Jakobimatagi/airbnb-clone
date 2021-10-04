892dbade048ff5063604e6467b35dab3234b8ed0
Jakobi Matagi <jkmatagi@hotmail.com>
2021-10-03 21:33:50 -0600

    created medium card

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import MediumCard from "../components/MediumCard";
+import SmallCard from "../components/SmallCard";
 
-export default function Home(props) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -19,20 +21,45 @@ export default function Home(props) {
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
           {/* Pull data from a server - API endpoints */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
         </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          {/* Pull data from a server - API endpoints */}
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title}) => (
+              <MediumCard key={img} img={img} title={title}/>
+            ))}
+          </div>
+        </section>
+
       </main>
     </div>
-  )
+  );
 }
 
-
 export async function getStaticProps() {
-  const exportData = await fetch('https://links.papareact.com/pyp').then(
-    res => res.json()
-    );
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+  const cardsData = await fetch("https://links.papareact.com/zp1").then(
+    (res) => res.json()
+  );
+
   return {
     props: {
-      exportData
-    }
-  }
+      exploreData,
+      cardsData,
+    },
+  };
 }
